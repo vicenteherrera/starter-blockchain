585656ddf061267ab21a2e245e937586a672495f
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2023-03-29 14:55:21 +0200

    feat: adding standard remix solidity examples

---
+++ workspace-eth/contracts/0_Amounts.sol
@@ -0,0 +1,15 @@
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract ExampleAmounts {
+
+    uint public value1 = 1 wei;
+    uint public value2 = 1;
+    uint public value3 = 1 gwei;
+    uint public value4 = 1000000000;
+    uint public value5 = 1 ether;
+    uint public value6 = 1000000000000000000;
+
+}
