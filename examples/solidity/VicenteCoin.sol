4b537f6fa187af3436b7077c00e510223bc2e8dc
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2024-04-23 10:30:18 +0200

    feat: added a dex Solidity example

--- examples/solidity/VicenteCoin.sol
+++ examples/solidity/VicenteCoin.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MyToken is ERC20, ERC20Permit {
+contract VicenteCoin is ERC20, ERC20Permit {
     constructor(uint256 initialSupply) ERC20("VicenteCoin", "VCC") ERC20Permit("VicenteCoin") {
         _mint(msg.sender, initialSupply);
     }   
