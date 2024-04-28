5bc2058f3fc9d75740c81b4ed785f069d8eff683
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2024-04-28 13:31:57 +0200

    feat: added dex that doesn't require ERC20 source

---
+++ examples/solidity/dex2.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Open in Remix IDE online with: https://remix.ethereum.org/vicenteherrera/starter-blockchain/blob/main/examples/solidity/dex2.sol
+
+// https://dev.to/emkay860/create-a-decentralized-exchange-dex-for-erc-20-token-kpm
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DEX {
+
+    IERC20 public token;
+
+    event Bought(uint256 amount);
+    event Sold(uint256 amount);
+
+    address vicenteCoinAddress = 0xad261586F4eD2f93185630D4F2f9a9DD02Ac72C2; //Sepolia
+
+    constructor() {
+        token = ERC20(vicenteCoinAddress);
+    }
+    function buy() payable public {
+        uint256 amountTobuy = msg.value;
+        uint256 dexBalance = token.balanceOf(address(this));
+        require(amountTobuy > 0, "You need to send some ether");
+        require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+        token.transfer(msg.sender, amountTobuy);
+        emit Bought(amountTobuy);
+    }
+    function sell(uint256 amount) public {
+        require(amount > 0, "You need to sell at least some tokens");
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+        payable(msg.sender).transfer(amount);
+        emit Sold(amount);
+    }
+}
